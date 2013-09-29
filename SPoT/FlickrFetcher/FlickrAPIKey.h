4de00bc5d90e0d116202b81ef5c3727f166083f9
Kyle Rogers <kyle.rogers@gmail.com>
2013-09-28 23:45:33 -0500

    init

---
+++ SPoT/FlickrFetcher/FlickrAPIKey.h
@@ -0,0 +1,12 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Winter 2013.
+//  Copyright 2013 Stanford University
+//  All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"4b79bc128f0035ef8b2d84bf1319ee8b"
