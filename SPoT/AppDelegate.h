146e3627c9ef2d1f815be1ce9ad7735123884e84
Kyle Rogers <kyle.rogers@gmail.com>
2013-09-28 23:40:38 -0500

    Initial Commit

---
+++ SPoT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SPoT
+//
+//  Created by Kyle Rogers on 9/28/13.
+//  Copyright (c) 2013 Kyle Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
